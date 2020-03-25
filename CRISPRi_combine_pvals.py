bca123c05e83d481a990659962eebc7805a59dc8
Tim Peterson <petersontimr@gmail.com>
2020-03-25 16:59:30 -0500

    1st commit of CADs code

---
+++ CRISPRi_combine_pvals.py
@@ -0,0 +1,43 @@
+#from pydoc import help
+import csv
+import sys
+import numpy as np
+import scipy
+from scipy.stats.stats import pearsonr
+
+from rpy2.robjects.packages import importr
+from rpy2.robjects.vectors import FloatVector
+
+stats = importr('stats')
+
+dataset = '/Users/timrpeterson/OneDrive-v2/Data/SSRIs/CADs_all_pvals.csv'
+
+with open(dataset) as csv_file:
+	csv_reader = csv.reader(csv_file, delimiter=",")
+	#next(csv_reader)
+
+	with open('/Users/timrpeterson/OneDrive-v2/Data/SSRIs/CADs_all_pvals_combined.csv', 'w') as csvfile:
+
+		spamwriter = csv.writer(csvfile, delimiter=',')
+	
+		genes = {}
+		for row in csv_reader:
+			gene = row[0]
+			row.pop(0)
+
+			p_adjust = stats.p_adjust(FloatVector(row), method = 'BH')
+			pval = scipy.stats.stats.combine_pvalues(p_adjust)
+
+		output = [gene, pval[1]]
+			#sort the output desc
+		output2 = sorted(output, key=lambda x: x[1]) #, reverse=True
+
+		spamwriter.writerows(output2)
+#		for row in output2:		
+			#if any(field.strip() for field in row):
+#			print(row)
+			#spamwriter.writerow(row)
+
+		csvfile.close()
+
+
