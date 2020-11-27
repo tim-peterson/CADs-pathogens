b92388334c74ca25d9456291845775c639984db6
tim-peterson <petersontimr@gmail.com>
2020-11-27 14:33:47 -0600

    4 figures comment #2

--- pathogens_transcriptional_profiling_count.R
+++ pathogens_transcriptional_profiling_count.R
@@ -2,7 +2,7 @@ library(ggplot2)
 library("scales")
 
 
-# Figure 4C
+# Figure 3C
 ggplot(pathogens_ge0, aes(x=cnt_all, y=cnt_all_0)) + geom_jitter(width = 0.1) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank()) + scale_x_continuous(trans='log2') + scale_y_continuous(trans='log2')
 
 # https://stackoverflow.com/questions/51159196/geom-jitter-colour-based-on-values
